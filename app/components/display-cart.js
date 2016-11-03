a50e97a93352c6ca7ae5add7add380b164a0c7a9
Tam Nguyen and Zachary Richards <zachary.j.richards@gmail.com>
2016-11-03 15:10:42 -0700

    add total to shopping cart

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
 
+  totalCost: Ember.computed('shoppingCart.items.length', function() {
+    var totalAmount = 0;
+    this.get('shoppingCart.items').forEach(function(item) {
+      totalAmount += parseInt(item.get('cost'));
+    });
+    return totalAmount;
+  }),
+
   actions: {
     removeFromCart(item) {
       this.get('shoppingCart').remove(item);
